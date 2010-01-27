5418b93541c91f5e72da17ada80bd65fe7ac97ac
Haim Ashkenazi <haim@babysnakes.org>
2010-01-28 00:04:15 +0200

    Added listing of available sections.
    The design is that when running without parameters you're getting a list
    of available sections. As you add sections to your parameters you'll get a
    list of available sub-sections. If there are no sub-sections, It's considered to
    be a host (we'll deal with that later).

--- src/babysnakes/runsshlib.py
+++ src/babysnakes/runsshlib.py
@@ -5,8 +5,34 @@
 #  Copyright 2010 Haim Ashkenazi. All rights reserved.
 # 
 
+import os, sys
+from configobj import ConfigObj
+
+_configfile = os.path.join(os.getenv('HOME'), '.runssh.conf')
+
 def main():
-    pass
+    args = sys.argv[1:]
+    config = ConfigObj(_configfile)
+    
+    # initialy we use the 'root' section
+    s = config
+    for arg in args:
+        try:
+            s = s[arg]
+        except KeyError, ke:
+            print 'invalid section: "%s"' % arg
+            sys.exit(5)
+    
+    # is this a group or host?
+    if s.sections:      # group
+        print_sections(s)
+    else:
+        pass
+            
+
+def print_sections(section):
+    for s in section.sections:
+        print s
 
 class RunSSH:
     pass
