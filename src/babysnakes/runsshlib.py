e846ad4ad552308c8d35470b574192839311e74c
Haim Ashkenazi <haim@babysnakes.org>
2010-02-23 08:49:38 +0200

    Added functionality to print configuration for a host.
    Also added docs, todo's and some workflow fixes.
    
    TODO:
    	- Modify runssh_comp.sh to also complete options.
    	- Test.

--- src/babysnakes/runsshlib.py
+++ src/babysnakes/runsshlib.py
@@ -5,6 +5,15 @@
 #  Copyright 2010 Haim Ashkenazi. All rights reserved.
 #
 
+"""
+TODO:
+    - Fix the workflow (e.g., The parser.error should only be thrown in one place
+      and we should use exceptions to handle it).
+    - Write validation for the config object (e.g., either section or host)
+    - Add functionality to edit (add/delete/create) configs through command line.
+    - Add ssh options (tunnel, port, etc)
+"""
+
 import os, sys
 from optparse import OptionParser
 from configobj import ConfigObj
@@ -17,12 +26,14 @@ def main():
     parser = OptionParser(usage=usage)
     parser.add_option("-l", action="store_true", dest="list", default=False,
                       help="list available sub-secions.")
+    parser.add_option("-p", action="store_true", dest="printconfig", default=False,
+                      help="print configuration of host definition.")
     (options, args) = parser.parse_args()
     
     config = ConfigObj(_configfile)
     
-    # initialy we use the 'root' section
-    s = config
+    # drill down through all sections (args)
+    s = config      # initialy we use the 'root' section
     for arg in args:
         try:
             s = s[arg]
@@ -36,14 +47,40 @@ def main():
         else:
             parser.error("last argument is a section, not host!")
     else:               # host
-        if not options.list:
+        if options.list:
+            pass    # list has no effect here.
+        if options.printconfig:
+            print_host(s)
+        else:
             run_ssh(s)
 
 def print_sections(section):
+    """
+    list available sub-secions
+    
+    arguments:
+        section - a configobj (sub)section to list it's contents.
+    """
     for s in section.sections:
         print s
 
+def print_host(host):
+    """
+    print the host configuration.
+    
+    arguments:
+        host - a configobj section with no sub-sections (only keys)
+    """
+    for k, v in host.items():
+        print "%s: %s" % (k, v)
+
 def run_ssh(section):
+    """
+    run ssh with the settings from the host (section).
+    
+    arguments:
+        section - The subsection with the host configuration.
+    """
     # TODO: why 'ssh' have to appear twice in execvp?
     sshargs = ['ssh']
     if 'user' in section.keys():
