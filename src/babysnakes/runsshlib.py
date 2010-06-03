d43e2eaef70c8f3d3994babb124ac5f603f2543a
Haim Ashkenazi <haim@babysnakes.org>
2010-06-03 11:19:58 +0300

    Started migrating the current functionality to cmdln.
    The first task would be to get the same functionality
    as before using cmdln tools. Then it should be easier
    to add more functionality.

--- src/babysnakes/runsshlib.py
+++ src/babysnakes/runsshlib.py
@@ -12,57 +12,49 @@ TODO:
     - Write validation for the config object (e.g., either section or host)
     - Add functionality to edit (add/delete/create) configs through command line.
     - Add ssh options (tunnel, port, etc)
+    - Handle '-' in section names (either avoid or solve completion)
 """
 
 import os, sys
-from optparse import OptionParser
+
+import cmdln
 from configobj import ConfigObj
 
-_configfile = os.path.join(os.getenv('HOME'), '.runssh.conf')
-
 def main():
+    command = RunSSHCmd()
+    sys.exit(command.main(loop=cmdln.LOOP_IF_EMPTY))
     # command line arguments
-    usage = "usage: %prog [options] [section ...] host"
-    parser = OptionParser(usage=usage)
-    parser.add_option("-l", action="store_true", dest="list", default=False,
-                      help="list available sub-secions.")
-    parser.add_option("-p", action="store_true", dest="printconfig", default=False,
-                      help="print configuration of host definition.")
-    (options, args) = parser.parse_args()
-    
-    config = ConfigObj(_configfile)
-    
-    # drill down through all sections (args)
-    s = config      # initialy we use the 'root' section
-    for arg in args:
-        try:
-            s = s[arg]
-        except KeyError, ke:
-            parser.error('invalid section: "%s"\n' % arg)
-    
-    # is this a group or host?
-    if s.sections:      # group
-        if options.list:
-            print_sections(s)
-        else:
-            parser.error("last argument is a section, not host!")
-    else:               # host
-        if options.list:
-            pass    # list has no effect here.
-        elif options.printconfig:
-            print_host(s)
-        else:
-            run_ssh(s)
-
-def print_sections(section):
-    """
-    list available sub-secions
-    
-    arguments:
-        section - a configobj (sub)section to list it's contents.
-    """
-    for s in section.sections:
-        print s
+    # usage = "usage: %prog [options] [section ...] host"
+    # parser = OptionParser(usage=usage)
+    # parser.add_option("-l", action="store_true", dest="list", default=False,
+    #                   help="list available sub-secions.")
+    # parser.add_option("-p", action="store_true", dest="printconfig", default=False,
+    #                   help="print configuration of host definition.")
+    # (options, args) = parser.parse_args()
+    #
+    # config = ConfigObj(_configfile)
+    #
+    # # drill down through all sections (args)
+    # s = config      # initialy we use the 'root' section
+    # for arg in args:
+    #     try:
+    #         s = s[arg]
+    #     except KeyError, ke:
+    #         parser.error('invalid section: "%s"\n' % arg)
+    #
+    # # is this a group or host?
+    # if s.sections:      # group
+    #     if options.list:
+    #         print_sections(s)
+    #     else:
+    #         parser.error("last argument is a section, not host!")
+    # else:               # host
+    #     if options.list:
+    #         pass    # list has no effect here.
+    #     elif options.printconfig:
+    #         print_host(s)
+    #     else:
+    #         run_ssh(s)
 
 def print_host(host):
     """
@@ -89,10 +81,69 @@ def run_ssh(section):
     try:
         host = section['host']
         sshargs.append(host)
-    except KeyError, ke:
+    except KeyError:
         # TODO: call this error through parser?
         sys.stderr.write('Invalid configuration for "%s". No host specified!\n'
                          % section.name)
         sys.exit(3)
     
     os.execvp('ssh', sshargs)
+
+
+class RunSSHConfig:
+    "A wrapper for the app configuration."
+    
+    def __init__(self, configfile=None):
+        if not configfile:
+            configfile = os.path.join(os.getenv('HOME'), '.runssh.conf')
+        self.config = ConfigObj(configfile)
+    
+    def get_subsections_list(self, path):
+        """ A utility method to return a list of available subsecions extracted
+            from the specified path.
+            
+            path is a sequence of ordered subsection names separated
+            by spaces. e.g, 'section1 subsection12 subsection124' where
+            subsecion12 is a sub-section of section1 etc...
+        """
+        s = self._get_section_from_path(path)
+        return s.sections
+    
+    def _get_section_from_path(self, path):
+        """ extract the section specified through the path.
+            
+            path is a sequence of ordered subsection names separated
+            by spaces. e.g, 'section1 subsection12 subsection124' where
+            subsecion12 is a sub-section of section1 etc...
+        """
+        s = self.config
+        for section in path:
+            try:
+                s = s[section]
+            except KeyError:
+                raise SectionError("No Such (sub)section: \"%s\"" % section)
+        return s
+
+
+class RunSSHCmd(cmdln.Cmdln):
+    
+    config = RunSSHConfig()
+    
+    def do_connect(self, subcmd, opts, *host):
+        "connect to remote host"
+        print 'connecting to',  host
+    
+    def complete_connect(self, text, line, begidx, endidx):
+        path = line.split()
+        # the first entry is the original command
+        path.pop(0)
+        if text:    # the word we're searching completion for
+            path.pop()
+        # no need to catch SectionError, cmdln ignores it.
+        result = RunSSHCmd.config.get_subsections_list(path)
+        return [n for n in result if n.startswith(text)]
+
+
+class SectionError(Exception):
+    """Indicates config section error."""
+    pass
