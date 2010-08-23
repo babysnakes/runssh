566453f4472727ee5868fbeff88d66b17d514514
Haim Ashkenazi <haim@babysnakes.org>
2010-08-23 11:04:46 +0300

    Started implementing the ConfigFile class.
    Also a few tests.

--- lib/runsshlib.rb
+++ lib/runsshlib.rb
@@ -1,4 +1,100 @@
 # Main RunSSHLib module.
 module RunSSHLib
   
+  DEFAULT_CONFIG = File.expand_path('~/.runssh')
+  
+  # Handles configuration file for the application.
+  # 
+  # The configuration consists of nested hashes which keys either
+  # points to another hash or to host definition.
+  # 
+  # The configuration file should use Marshal to save/load
+  #  configuration, but should also be able to import/export
+  # to/from yaml file.
+  class ConfigFile
+    
+    def initialize(config_file = DEFAULT_CONFIG)
+      @config_file = config_file
+      if File.exists? config_file
+        File.open(config_file) { |io| @config = Marshal.load(io) }
+      else
+        warn "Config file not found. It must be the first time you run this app..."
+        @config = Hash.new
+        save
+      end
+    end
+    
+    def add_host_def(path, host_def)
+    end
+    
+    # Update host definition (host_def) at the specified path.
+    # Raises ConfigError if doesn't already exist!
+    def update_host_def(path, host_def)
+      # sanity
+      raise ConfigError.new('Invalid host definition!') if not 
+            host_def.instance_of? HostDef
+      
+      host = get_host(path)
+      host = host_def
+      save
+    end
+    
+    # Returns the host definition in the specified path.
+    # `path` is an array of symbols which translates to nested hash keys.
+    # Raises ConfigError if not found or if path points to a group.
+    def get_host(path)
+      host = path.inject(@config) { |hsh, key| hsh[key] }
+      
+      if not host
+        raise ConfigError.new(%Q{host definition (#{path.join(' => ')}) doesn't exist!})
+      elsif host.instance_of? Hash
+        raise ConfigError.new(%Q("#{path.join(' => ')}" is a group, not host definition!))
+      end
+      
+      host
+    end
+    
+    def list_groups(path)
+      value = path.inject(@config) { |hsh, key| hsh[key] }
+      
+      # handle invalid path
+      raise ConfigError.new(%Q("#{path.join(' => ')}" doesn't exist!')) unless value
+      
+      if value.instance_of? Hash
+        value.keys
+      else
+        nil
+      end
+    end
+    
+    # Export config as YAML to the supplied file.
+    def import(file)
+      require 'yaml'
+      @config = YAML.load_file(file)
+      save
+    end
+    
+    # Import config from YAML from the specified file.
+    def export(file)
+      require 'yaml'
+      File.open(file) { |out| YAML.dump(@config, out) }
+    end
+
+    private
+    
+    def save
+      require 'ftools'
+      # create backup (File.copy always seems to overwrite existing file)
+      File.copy(@config_file, @config_file + '.bak') if File.exists? @config_file
+      File.open(@config_file, 'w') { |out| Marshal.dump(@config, out) }
+    end
+    0
+  end
+
+  # Indicates configuration error
+  class ConfigError < RuntimeError
+  end
+  
+  # A placeholder for host definitions
+  HostDef = Struct.new(:name, :login)
 end
--- test/config_spec.rb
+++ test/config_spec.rb
@@ -1,4 +1,38 @@
+require 'lib/runsshlib'
+require 'tmpdir'
+
 describe "RunSSH Configuration class" do
+
+  def read_config
+    File.open(@temp_file) { |io| Marshal.load(io) }
+  end
+
+  before(:all) do
+    @temp_file = File.join(Dir.tmpdir, 'tempfile')
+    @temp_file_bak = @temp_file + '.bak'
+  end
+  
+  it "should save a new empty configuration if none exists" do
+    RunSSHLib::ConfigFile.new(@temp_file)
+    read_config.should == {}
+  end
+  it "should create a backup while saving" do
+    c = RunSSHLib::ConfigFile.new(@temp_file)
+    c.send(:save)
+    # the 2 files should match
+    File.read(@temp_file).should == File.read(@temp_file_bak)
+  end
+  it "should overwrite existing backup if one already exists"
   it "should accept array of nested hashes with HostDef as the last value" 
   it "should return HostDef from an array path"
+  
+  after(:each) do
+    if File.exists? @temp_file
+      File.delete(@temp_file)
+    end
+    if File.exists? @temp_file_bak
+      File.delete(@temp_file_bak)
+    end
+  end
+  
 end
