875952c0a2069764744cc4ae31eb072ce2f80ec5
Haim Ashkenazi <haim@babysnakes.org>
2010-08-24 18:49:15 +0300

    Fixed the get_host method.
    It would fail if more then the last element of the
    path was invalid. Fixed and tested.

--- lib/runsshlib.rb
+++ lib/runsshlib.rb
@@ -1,20 +1,20 @@
 # Main RunSSHLib module.
 module RunSSHLib
-  
+
   DEFAULT_CONFIG = File.expand_path('~/.runssh')
-  
+
   # Handles configuration file for the application.
-  # 
+  #
   # The configuration consists of nested hashes which keys either
   # points to another hash or to host definition.
-  # 
+  #
   # The configuration file should use Marshal to save/load
   #  configuration, but should also be able to import/export
   # to/from yaml file.
   class ConfigFile
-    
+
     # Initialize new ConfigFile. Uses supplied config_file or the default
-    # '~/.runssh'. If file doesn't exist, it issues a warning and creates 
+    # '~/.runssh'. If file doesn't exist, it issues a warning and creates
     # a new empty one.
     def initialize(config_file = DEFAULT_CONFIG)
       @config_file = config_file
@@ -26,7 +26,7 @@ module RunSSHLib
         save
       end
     end
-    
+
     # Add host definition to config file.
     # `path`: An array of symbols that represent the path
     #         for the host. e.g, [:client, :datacenter1].
@@ -35,7 +35,7 @@ module RunSSHLib
     def add_host_def(path, name, host_def)
       # sanity
       raise ConfigError.new('Invalid host definition') unless host_def.instance_of? HostDef
-      
+
       k = path.inject(@config) do |hsh, key|
         if hsh.include? key
           if hsh[key].instance_of? HostDef
@@ -46,20 +46,20 @@ module RunSSHLib
           hsh[key] = {}
         end
       end
-      
+
       raise ConfigError.new('path already exist!') if k.include? name
-      
+
       k[name] = host_def
       save
     end
-    
+
     # Update host definition (host_def) at the specified path.
     # Raises ConfigError if doesn't already exist!
     def update_host_def(path, host_def)
       # sanity
-      raise ConfigError.new('Invalid host definition!') if not 
+      raise ConfigError.new('Invalid host definition!') if not
             host_def.instance_of? HostDef
-      
+
       # we need to separate the host name from the path
       # in order to get the key of the host definition.
       host = path.pop
@@ -76,42 +76,46 @@ module RunSSHLib
       end
       save
     end
-    
+
     # Returns the host definition in the specified path.
     # `path` is an array of symbols which translates to nested hash keys.
     # Raises ConfigError if not found or if path points to a group.
     def get_host(path)
-      host = path.inject(@config) { |hsh, key| hsh[key] }
-      
+      host = path.inject(@config) do |hsh, ky|
+        raise ConfigError.new(
+              %Q{host definition (#{path.join(' => ')}) doesn't exist!}) unless hsh
+        hsh[ky]
+      end
+
       if not host
         raise ConfigError.new(%Q{host definition (#{path.join(' => ')}) doesn't exist!})
       elsif host.instance_of? Hash
         raise ConfigError.new(%Q("#{path.join(' => ')}" is a group, not host definition!))
       end
-      
+
       host
     end
-    
+
     def list_groups(path)
       value = path.inject(@config) { |hsh, key| hsh[key] }
-      
+
       # handle invalid path
       raise ConfigError.new(%Q("#{path.join(' => ')}" doesn't exist!')) unless value
-      
+
       if value.instance_of? Hash
         value.keys
       else
         nil
       end
     end
-    
+
     # Export config as YAML to the supplied file.
     def import(file)
       require 'yaml'
       @config = YAML.load_file(file)
       save
     end
-    
+
     # Import config from YAML from the specified file.
     def export(file)
       require 'yaml'
@@ -119,20 +123,19 @@ module RunSSHLib
     end
 
     private
-    
+
     def save
       require 'ftools'
       # create backup (File.copy always seems to overwrite existing file)
       File.copy(@config_file, @config_file + '.bak') if File.exists? @config_file
       File.open(@config_file, 'w') { |out| Marshal.dump(@config, out) }
     end
-    0
   end
 
   # Indicates configuration error
   class ConfigError < RuntimeError
   end
-  
+
   # A placeholder for host definitions
   HostDef = Struct.new(:name, :login)
 end
--- test/config_spec.rb
+++ test/config_spec.rb
@@ -6,10 +6,10 @@ describe "RunSSH Configuration class" do
   def read_config
     File.open(@temp_file) { |io| Marshal.load(io) }
   end
-  
+
   def initial_data
     @c = RunSSHLib::ConfigFile.new(@temp_file)
-    @c.add_host_def([:one, :two, :three], :www, 
+    @c.add_host_def([:one, :two, :three], :www,
                    RunSSHLib::HostDef.new('www.example.com', 'me'))
   end
 
@@ -19,47 +19,57 @@ describe "RunSSH Configuration class" do
     @h1 = RunSSHLib::HostDef.new('a.b.c')
     @h2 = RunSSHLib::HostDef.new('b.b.c', 'meme')
   end
-  
+
   it "should save a new empty configuration if none exists" do
     RunSSHLib::ConfigFile.new(@temp_file)
     read_config.should == {}
   end
-  
+
   it "should create a backup while saving" do
     c = RunSSHLib::ConfigFile.new(@temp_file)
     c.send(:save)
     # the 2 files should match
     File.read(@temp_file).should == File.read(@temp_file_bak)
   end
-  
-  it "should overwrite existing backup if one already exists"
-  
+
+  it "should overwrite existing backup if one already exists" do
+    # create a new file and a copy of it
+    c = RunSSHLib::ConfigFile.new(@temp_file)
+    c.send(:save)
+    # sanity
+    File.read(@temp_file).should == File.read(@temp_file_bak)
+    b = RunSSHLib::ConfigFile.new(@temp_file)
+    b.add_host_def([:one, :two, :three], :www, @h1)
+    File.read(@temp_file).should_not == File.read(@temp_file_bak)
+
+  end
+
   describe "when adding host" do
     it "should not overwrite existing paths with host definition" do
       initial_data
       lambda {@c.add_host_def([:one, :two], :three, @h1)}.
               should raise_exception(RunSSHLib::ConfigError, /path already exist/)
     end
-    
+
     it "should not overwrite existing hosts with path" do
       initial_data
       lambda {@c.add_host_def([:one, :two, :three, :www], :host, @h1)}.
-              should raise_exception(RunSSHLib::ConfigError, 
+              should raise_exception(RunSSHLib::ConfigError,
                                      /Cannot override host definition with path/)
     end
-    
+
     it "should not overwrite existing hosts" do
       initial_data
       lambda {@c.add_host_def([:one, :two, :three], :www, @h1)}.
               should raise_exception(RunSSHLib::ConfigError, /path already exist/)
     end
-    
+
     it "should fail if invalid host definition" do
       initial_data
       lambda { @c.add_host_def([], :host, :error) }.
               should raise_error(RunSSHLib::ConfigError, /Invalid host definition/)
     end
-    
+
     it "should correctly merge different paths" do
       c = RunSSHLib::ConfigFile.new(@temp_file)
       c.add_host_def([:one, :two], :h1, @h1)
@@ -68,7 +78,7 @@ describe "RunSSH Configuration class" do
       d.get_host([:one, :two, :h1]).should == @h1
       d.get_host([:three, :four, :h2]).should == @h2
     end
-    
+
     it "should correctly merge paths with common path" do
       c = RunSSHLib::ConfigFile.new(@temp_file)
       c.add_host_def([:one, :two], :h1, @h1)
@@ -77,7 +87,7 @@ describe "RunSSH Configuration class" do
       d.get_host([:one, :two, :h1]).should == @h1
       d.get_host([:one, :three, :h2]).should == @h2
     end
-    
+
     it "should correctly merge hosts under same path" do
       c = RunSSHLib::ConfigFile.new(@temp_file)
       c.add_host_def([:one, :two], :h1, @h1)
@@ -87,44 +97,70 @@ describe "RunSSH Configuration class" do
       d.get_host([:one, :two, :h2]).should == @h2
     end
   end
-  
+
   describe "when updating host" do
     it "should refuse to *update* non-existing host" do
       initial_data
       lambda { @c.update_host_def([:one, :two, :host], @h1) }.
-              should raise_error(RunSSHLib::ConfigError, 
+              should raise_error(RunSSHLib::ConfigError,
                                  /Host definition doesn't exist/)
     end
-    
+
     it "should refuse to accept invalid host definition" do
       initial_data
       lambda { @c.update_host_def([:one, :two, :three, :www], :wrong) }.
               should raise_error(RunSSHLib::ConfigError, /Invalid/)
     end
-    
+
     it "should NOT replace path with host definition" do
       initial_data
       lambda { @c.update_host_def([:one, :two, :three], @h2) }.
               should raise_error(RunSSHLib::ConfigError, /Cannot overwrite/)
-      
+
     end
-    
+
     it "should correctly update and save host definition" do
       initial_data
       @c.update_host_def([:one, :two, :three, :www], @h2)
       c = RunSSHLib::ConfigFile.new(@temp_file)
       c.get_host([:one, :two, :three, :www]).should == @h2
     end
-    
+
     it "should handle invalid paths correctly" do
       initial_data
       lambda { @c.update_host_def([:two, :three, :four], @h2) }.
               should raise_error(RunSSHLib::ConfigError, /Invalid/)
     end
   end
-  
-  it "should return HostDef from an array path"
-  
+
+  describe "when quering for host" do
+    it "should raise error if requested host is path" do
+      initial_data
+      lambda { @c.get_host([:one, :two]) }.
+              should raise_error(RunSSHLib::ConfigError,
+                                 /is a group, not host definition/)
+    end
+
+    it "should raise error if requested host doesn't exist" do
+      initial_data
+      lambda { @c.get_host([:one, :two, :three, :www2]) }.
+              should raise_error(RunSSHLib::ConfigError, /oesn't exist!/)
+    end
+
+    it "should raise the correct error even if the path is completely invalid" do
+      initial_data
+      lambda { @c.get_host([:dummy, :two, :three, :www]) }.
+              should raise_error(RunSSHLib::ConfigError, /oesn't exist!/)
+    end
+
+    it "should return the correct host when the path is right" do
+      c = RunSSHLib::ConfigFile.new(@temp_file)
+      c.add_host_def([:customer, :dc], :host1, @h1)
+      b = RunSSHLib::ConfigFile.new(@temp_file)
+      b.get_host([:customer, :dc, :host1]).should == @h1
+    end
+  end
+
   after(:each) do
     if File.exists? @temp_file
       File.delete(@temp_file)
@@ -133,5 +169,5 @@ describe "RunSSH Configuration class" do
       File.delete(@temp_file_bak)
     end
   end
-  
+
 end
