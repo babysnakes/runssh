89231c2f422e2b52940cd6d0748e8b54c35aebf3
Haim Ashkenazi <haim@babysnakes.org>
2010-08-29 19:16:53 +0300

    Added adaptation to the previous bash-completion file.
    This adaptation completes the path but not the commands.
    To be continued ...

---
+++ bin/runssh_comp.sh
@@ -0,0 +1,11 @@
+function _runssh () {
+    local cur options
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    options=$(${COMP_WORDS[@]:0:COMP_CWORD} ? 2>/dev/null)
+    COMPREPLY=( $(compgen -W "${options}" -- ${cur}) )
+    return 0
+}
+
+complete -F _runssh bin/runssh
+complete -F _runssh runssh
