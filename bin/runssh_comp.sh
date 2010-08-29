be4b2083572e06461460d79bd40968ca5885de91
Haim Ashkenazi <haim@babysnakes.org>
2010-08-29 20:01:44 +0300

    Added bash-completions for commands.
    Need to handle cases where current word starts with -*

--- bin/runssh_comp.sh
+++ bin/runssh_comp.sh
@@ -1,8 +1,22 @@
 function _runssh () {
-    local cur options
+    local cur options COM_POSITION
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
-    options=$(${COMP_WORDS[@]:0:COMP_CWORD} ? 2>/dev/null)
+    echo ${COMP_WORDS[1]} >result
+    # where is the COMMAND part of the argument (are we using -f?)
+    COM_POSITION=1
+    if [ ${COMP_WORDS[1]} ]; then
+        if [ ${COMP_WORDS[1]} = '-f' ]; then
+            COM_POSITION=3
+        fi
+    fi
+    # complete path or commands according to the position.
+    if [ ${COMP_CWORD} -gt $COM_POSITION ]; then
+        options=$(${COMP_WORDS[@]:0:COMP_CWORD} ? 2>/dev/null)
+    elif [ $COMP_CWORD -eq $COM_POSITION ]; then
+        options="shell add del print import export"
+    fi
+    echo "$COM_POSITION, $COMP_CWORD, $options" >result
     COMPREPLY=( $(compgen -W "${options}" -- ${cur}) )
     return 0
 }
