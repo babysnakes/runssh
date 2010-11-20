fff52125455b1a27ecedb97a7f1da16bc43c8320
Haim Ashkenazi <haim@babysnakes.org>
2010-11-20 07:14:24 +0200

    New SshHostDef class.
    This will replace the old HostDef class.

--- lib/runsshlib/ssh_host_def.rb
+++ lib/runsshlib/ssh_host_def.rb
@@ -1,46 +1,28 @@
 #
 # Copyright (C) 2010 Haim Ashkenazi
-#
+# 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-#
+# 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
-
-require 'runsshlib/cli'
-require 'runsshlib/config_file'
-require 'runsshlib/ssh_backend'
-require 'runsshlib/ssh_host_def'
-require 'highline'
-
-# Main RunSSHLib module.
+# 
 module RunSSHLib
-
-  DEFAULT_CONFIG = File.expand_path('~/.runssh')
-
-  # Indicates configuration error
-  class ConfigError < StandardError; end
-
-  # Indicates invalid command
-  class InvalidSubCommandError < StandardError; end
-
-  # A placeholder for host definitions
-  HostDef = Struct.new(:name, :login)
-
-  module Version
-    MAJOR = 0
-    MINOR = 2
-    BUILD = 0
-
-    STRING = [MAJOR, MINOR, BUILD].compact.join('.')
+  class SshHostDef
+    # Initialize ssh host def with definition.
+    # 
+    # [definition] A hash containing ssh options. _host_name_ is required.
+    def initialize(definition)
+      raise ArgumentError unless definition[:host_name]
+      @definition = definition
+    end
   end
 end
--- spec/runsshlib/ssh_host_def_spec.rb
+++ spec/runsshlib/ssh_host_def_spec.rb
@@ -1,46 +1,35 @@
 #
 # Copyright (C) 2010 Haim Ashkenazi
-#
+# 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-#
+# 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'lib/runsshlib'
 
-require 'runsshlib/cli'
-require 'runsshlib/config_file'
-require 'runsshlib/ssh_backend'
-require 'runsshlib/ssh_host_def'
-require 'highline'
-
-# Main RunSSHLib module.
-module RunSSHLib
-
-  DEFAULT_CONFIG = File.expand_path('~/.runssh')
-
-  # Indicates configuration error
-  class ConfigError < StandardError; end
-
-  # Indicates invalid command
-  class InvalidSubCommandError < StandardError; end
-
-  # A placeholder for host definitions
-  HostDef = Struct.new(:name, :login)
-
-  module Version
-    MAJOR = 0
-    MINOR = 2
-    BUILD = 0
-
-    STRING = [MAJOR, MINOR, BUILD].compact.join('.')
+describe 'SshHostDef' do
+  describe 'when initialized' do
+    it "should raise ArgumentError if not initialized with host_name" do
+      expect {
+        RunSSHLib::SshHostDef.new({:login => 'haim'})
+      }.to raise_error(ArgumentError)
+    end
+    
+    it "should initialize correctly if at least host_name exists" do
+      definition = {:host_name => 'myname'}
+      shd = RunSSHLib::SshHostDef.new(definition)
+      shd.instance_variable_get(:@definition).should eql(definition)
+    end
   end
 end
