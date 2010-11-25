c05de298474d4921f9d5827802d897aa40027938
Haim Ashkenazi <haim@babysnakes.org>
2010-11-25 21:15:48 +0200

    Definied equality for SshHostDef
    I'm not sure if I really need it except for tests :)

--- lib/runsshlib/ssh_host_def.rb
+++ lib/runsshlib/ssh_host_def.rb
@@ -1,28 +1,46 @@
 #
 # Copyright (C) 2010 Haim Ashkenazi
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-# 
+#
 module RunSSHLib
   class SshHostDef
+    attr_reader :definition
+
     # Initialize ssh host def with definition.
-    # 
-    # [definition] A hash containing ssh options. _host_name_ is required.
+    #
+    # [definition] A hash containing ssh options. <i>:host_name</i> is required.
+    #              Could also be a hostname (string) for quick defining SshHostDef
+    #              with only hostname.
     def initialize(definition)
+      if definition.instance_of? String
+        definition = { :host_name => definition }
+      end
       raise ArgumentError, "Missing hostname" unless definition[:host_name]
       @definition = definition
+    end
+
+    # should be equal if @definition is equal
+    def ==(other)
+      return false if other.nil?
+      return false unless other.instance_of? SshHostDef
+      definition == other.definition
+    end
+
+    def eql?(other)
+      self == other
     end
   end
 end
--- spec/runsshlib/ssh_host_def_spec.rb
+++ spec/runsshlib/ssh_host_def_spec.rb
@@ -1,16 +1,16 @@
 #
 # Copyright (C) 2010 Haim Ashkenazi
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -19,17 +19,58 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require 'lib/runsshlib'
 
 describe 'SshHostDef' do
-  describe 'when initialized' do
+  context 'when initialized' do
+    let(:data) { {:host_name => 'myname'} }
+
     it "should raise ArgumentError if not initialized with host_name" do
       expect {
         RunSSHLib::SshHostDef.new({:login => 'haim'})
       }.to raise_error(ArgumentError, /Missing hostname/)
     end
-    
-    it "should initialize correctly if at least host_name exists" do
-      definition = {:host_name => 'myname'}
-      shd = RunSSHLib::SshHostDef.new(definition)
-      shd.instance_variable_get(:@definition).should eql(definition)
+
+    it "should initialize correctly if supplied a hostname as string" do
+      shd = RunSSHLib::SshHostDef.new('myname')
+      shd.instance_variable_get(:@definition).should eql(data)
+    end
+
+    it "should initialize correctly if supplied with a hash with :host_name" do
+      shd = RunSSHLib::SshHostDef.new(data)
+      shd.instance_variable_get(:@definition).should eql(data)
+    end
+  end
+
+  context 'when testing for equality' do
+    let(:definition) do
+      { :host_name => 'host', :login => "login" }
+    end
+
+    it "should not equal nil" do
+      RunSSHLib::SshHostDef.new(definition).should_not == nil
+    end
+
+    it "should not equal any object" do
+      class MyTest
+        attr_reader :definition
+        def initialize definition
+          @definition = definition
+        end
+      end
+
+      RunSSHLib::SshHostDef.new(definition).should_not == MyTest.new(definition)
+    end
+
+    it "should return true for == if @definition is equal" do
+      h1 = RunSSHLib::SshHostDef.new(definition)
+      h2 = RunSSHLib::SshHostDef.new('hostname')
+      RunSSHLib::SshHostDef.new(definition).should == h1
+      RunSSHLib::SshHostDef.new('hostname').should == h2
+    end
+
+    it "should return true for eql? if @definition is equal" do
+      h1 = RunSSHLib::SshHostDef.new(definition)
+      h2 = RunSSHLib::SshHostDef.new('hostname')
+      RunSSHLib::SshHostDef.new(definition).should eql(h1)
+      RunSSHLib::SshHostDef.new('hostname').should eql(h2)
     end
   end
 end
